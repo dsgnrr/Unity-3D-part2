f84ab123fd442ad93d0702f4c4f044bb3bfe4ae0
dsgnrr <semeniuk.oleg2002@gmail.com>
2024-02-09 14:49:36 +0200

    Lesson 2. Charachter moving.

---
+++ Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 100f;
+    private CharacterController _characterController;
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        float dx = Input.GetAxis("Horizontal");
+        float dy = Input.GetAxis("Vertical");
+        if (Mathf.Abs(dx) > 0 && Mathf.Abs(dy) > 0)
+        {
+            dx *= 0.707f; // /= Mathf.Sqrt(2f);
+            dy *= 0.707f; // /= Mathf.Sqrt(2f);
+        }
+        _characterController.SimpleMove(
+            speed *
+            Time.deltaTime *
+            //new Vector3(dx, 0, dy) - світові координати
+            (dx * Camera.main.transform.right +
+            dy * Camera.main.transform.forward)
+            );
+    }
+}
